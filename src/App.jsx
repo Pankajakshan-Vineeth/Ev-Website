34330f4b8837692b62c4d8a94266394adb15c808
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-05-04 20:12:06 +0530

    background wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Background from './Components/Background/Backgroound'; // fix typo if needed
+
+const heroData = [
+  { text1: "Dive into", text2: "what you love" },
+  { text1: "Indulge", text2: "your passions" },
+  { text1: "Give in to", text2: "your passions" },
+];
 
 const App = () => {
+
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
   return (
     <div>
-      Hi vineeth
-      
+      <Background heroCount={heroCount} playStatus={playStatus} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
